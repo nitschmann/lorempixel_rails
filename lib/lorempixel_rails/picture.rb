b6ffa92f1d144913645bba65a79a2d319efb5163
Florian Nitschmann <f.nitschmann@googlemail.com>
2014-01-14 11:03:19 +0100

    stable version of picture class

--- lib/lorempixel_rails/picture.rb
+++ lib/lorempixel_rails/picture.rb
@@ -3,5 +3,71 @@ module LorempixelRails
     require "lorempixel_rails/picture/uri"
     require "lorempixel_rails/picture/type"
     require "lorempixel_rails/picture/size"
+
+    class << self
+      def new(options = {})
+        set_uri
+        set_size(options)
+        set_type(options)
+        set_text(options)
+        set_color(options)
+        build_picture_uri
+      end
+
+      private
+      def build_picture_uri
+        uri = "#{@uri}"
+        if @gray == true
+          uri = "#{uri}g/"
+        end
+        uri = "#{uri}#{@size}/#{@type}"
+        if instance_variable_defined?(:@text)
+          uri = "#{uri}/#{@text}"
+        end
+        uri
+      end
+
+      def set_uri
+        @uri = LorempixelRails::Picture::URI
+      end
+
+      def set_size(options = {})
+        if options.has_key?(:size) && options[:size].kind_of?(Hash)
+          size_hash = options[:size]
+          if size_hash.has_key?(:width) && size_hash.has_key?(:height)
+            @size = LorempixelRails::Picture::Size.new(size_hash[:width].to_i, size_hash[:height].to_i)
+          else
+            raise ArgumentError, "width and height need to be set"
+          end
+        elsif options.has_key?(:size) && options[:size] == "random"
+          @size = LorempixelRails::Picture::Size.random
+        else
+          @size = LorempixelRails::Picture::Size.new
+        end
+      end
+
+      def set_type(options = {})
+        type = nil
+        type = options[:type] if options.has_key?(:type)
+        if type == "random"
+          @type = LorempixelRails::Picture::Type.random
+        else
+          @type = LorempixelRails::Picture::Type.new(type)
+        end
+      end
+
+      def set_text(options = {})
+        if options.has_key?(:text)
+           @text = options[:text]
+        end
+      end
+
+      def set_color(options = {})
+        @gray = false
+        if options.has_key?(:gray) && options[:gray] == true
+          @gray = true
+        end
+      end
+    end
   end
 end
--- spec/lorempixel_rails/picture_spec.rb
+++ spec/lorempixel_rails/picture_spec.rb
@@ -1,13 +1,81 @@
 require "spec_helper"
 
 describe LorempixelRails::Picture do
-  context "#URI" do
-    specify do
-      expect(LorempixelRails::Picture::URI).to be
+  describe "#new" do
+    context "with specific size and type of given list" do
+      let(:options) {
+        {
+          :size => {:width => 300, :height => 200},
+          :type => "animals"
+        }
+      }
+      let!(:picture) { LorempixelRails::Picture.new(options) }
+
+      it { expect(picture).to be }
+      it { expect(picture).to be_kind_of(String) }
+      it { expect(picture).to eql("http://lorempixel.com/300/200/animals") }
     end
 
-    specify do
-      expect(LorempixelRails::Picture::URI).to eql("http://lorempixel.com/")
+    context "with specific size, type of given list and in gray" do
+      let(:type) { "animals" }
+      let(:options) {
+        {
+          :size => {:width => 300, :height => 200},
+          :type => type,
+          :gray => true
+        }
+      }
+      let!(:picture) { LorempixelRails::Picture.new(options) }
+
+      it { expect(picture).to be }
+      it { expect(picture).to be_kind_of(String) }
+      it { expect(picture).to eql("http://lorempixel.com/g/300/200/#{type}") }
+    end
+
+    context "without specific size and with type of given list" do
+      let(:type) { "abstract" }
+      let(:options) { {:type => type} }
+      let!(:picture) { LorempixelRails::Picture.new(options) }
+
+      it { expect(picture).to be }
+      it { expect(picture).to be_kind_of(String) }
+      it { expect(picture).to eql("http://lorempixel.com/100/100/#{type}") }
+    end
+
+    context "without specific size, with type of given list and in gray" do
+      let(:type) { "abstract" }
+      let(:options) { {:type => type, :gray => true} }
+      let!(:picture) { LorempixelRails::Picture.new(options) }
+
+      it { expect(picture).to be }
+      it { expect(picture).to be_kind_of(String) }
+      it { expect(picture).to eql("http://lorempixel.com/g/100/100/#{type}") }
+    end
+
+    context "with wrong size hash"  do
+      let(:options) { {:size => {}} }
+
+      it { expect{ LorempixelRails::Picture.new(options) }.to raise_error(ArgumentError) }
+      it {
+        expect{ LorempixelRails::Picture.new(options) }.to raise_error(ArgumentError, "width and height need to be set")
+      }
+    end
+
+    context "with specific size and without type" do
+      let(:options) { {:size => {:width => 300, :height => 200}} }
+      let!(:picture) { LorempixelRails::Picture.new(options) }
+
+      it { expect(picture).to be }
+      it { expect(picture).to be_kind_of(String) }
+      it { expect(picture).to eql("http://lorempixel.com/300/200/nature") }
+    end
+
+    context "without specific size and without type" do
+      let!(:picture) { LorempixelRails::Picture.new }
+
+      it { expect(picture).to be }
+      it { expect(picture).to be_kind_of(String) }
+      it { expect(picture).to eql("http://lorempixel.com/100/100/nature") }
     end
   end
 end
